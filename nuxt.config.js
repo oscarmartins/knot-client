cc22bd8a2993eedef2e891856016d00d9af5ccc9
syropian <collinpatrickhenderson@gmail.com>
2017-08-14 22:31:26 -0400

    Adds mobile-web-app-capable meta tag, disables user scaling

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ module.exports = {
     title: 'Knot',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
